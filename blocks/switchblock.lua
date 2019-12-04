c7338b6a49effceee39d9e9bf440ff256b3e915f
chiud3UCI <chiud3@uci.edu>
2019-12-03 22:52:19 -0800

    Switchblocks are now synced with each other

--- blocks/switchblock.lua
+++ blocks/switchblock.lua
@@ -15,27 +15,32 @@ function SwitchBlock:initialize(i, j, switchColor)
     Block.initialize(self, i, j)
     --toggle determines whether or not block can be
     --toggled on/off or just stay on
-    self.toggle = true
 	self.imgstr = "switch_off"
     self.on = false
     self.switchColor = switchColor
     local sc = SwitchBlock.colors[switchColor]
     self.color = {r = sc[1], g = sc[2], b = sc[3], a = 1}
     self.touched = 0
+
+    self.blockType = "Switch"
+end
+
+function SwitchBlock:touchOtherSwitches()
+    for _, t in ipairs(game.tiles) do
+        if t ~= self then
+            if t.blockType == "Switch" and t.switchColor == self.switchColor then
+                t.touched = 2
+            end
+        end
+    end
 end
 
 function SwitchBlock:onPlayerHit(player)
     if self.touched == 0 then
-        if self.toggle then
-            self.on = not self.on
-            self:onSwitch()
-        else
-            if not self.on then
-                self.on = true
-                self:onSwitch()
-            end
-        end
+        self.on = not self.on
+        self:onSwitch()
     end
+    self:touchOtherSwitches()
     self.touched = 2
 end
 
@@ -45,8 +50,13 @@ end
 
 function SwitchBlock:onSwitch()
     for _, t in ipairs(game.tiles) do
-        if t.actuator == self.switchColor then
-            t:onTrigger()
+        if t ~= self then
+            if t.actuator == self.switchColor then
+                t:onTrigger()
+            end
+            if t.blockType == "Switch" and t.switchColor == self.switchColor then
+                t.on = not t.on
+            end
         end
     end
 end
